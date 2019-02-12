279d306fe739a0d360ca457946e7d531eff929b2
AbhikChowdhury6 <abhik.ch6@gmail.com>
2019-02-12 08:58:24 -0700

    added another arbitrary parameter

--- VideoStream.sh
+++ VideoStream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #mplayer -fps 30 -demuxer h264es ffmpeg://tcp://192.168.1.102:2222
-raspivid -t 0 -n -w 1280 -h 720 -hf -ih -fl $2 -fps 20 -o - | nc -k -l $1
+raspivid -t 0 -n -w 1280 -h 720 $3 -ih -fl $2 -fps 20 -o - | nc -k -l $1
