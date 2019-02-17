06d0b7f30c8a1ad410960011418afc1ba3377059
AbhikChowdhury6 <abhik.ch6@gmail.com>
2019-02-17 15:54:44 +0000

    modified stitcher for new time format to test

--- analysis/cvStitchButReally.py
+++ analysis/cvStitchButReally.py
@@ -1,17 +1,14 @@
 import cv2
 import numpy as np
 import sys
-import datetime
 import time
 
-vid1StartTime = datetime.datetime(2019, 2, 15, 23, 9, 32)
-print(time.mktime(vid1StartTime.timetuple()))
+vid1 = sys.argv[1]
+vid2 = sys.argv[2]
 
-vid1 = "/home/chowder/tp1/handcam-camera-15-02-2019-23-09-32.157437548.mp4"
-vid2 = "/home/chowder/hc1/handcam-camera-15-02-2019-23-09-29.511219310.mp4"
+vid1StartTime = long(vid1.split("-")[0])
+vid2StartTime = long(vid2.split("-")[0])
 
-vid1StartTime = vid1.split("-")
-vid2StartTime = vid2.split("-")
 #import videos as captures
 #import both videos as arrays of frames
 #get timestamp arrays for both captures
@@ -23,7 +20,7 @@ while(cap1.isOpened()):
     frame_exists, curr_frame = cap1.read()
     if frame_exists:
         vid1i.append(curr_frame)
-        vid1Timestamps.append(cap1.get(cv2.CAP_PROP_POS_MSEC))
+        vid1Timestamps.append(vid1StartTime + cap1.get(cv2.CAP_PROP_POS_MSEC) * 10000000)
     else:
         break
 
@@ -32,14 +29,13 @@ cap1.release()
 
 vid2i = []
 vid2Timestamps = []
-#cap2 = cv2.VideoCapture(sys.argv[2])
 cap2 = cv2.VideoCapture(vid2)
 
 while(cap2.isOpened()):
     frame_exists, curr_frame = cap2.read()
     if frame_exists:
         vid2i.append(curr_frame)
-        vid2Timestamps.append(cap2.get(cv2.CAP_PROP_POS_MSEC))
+        vid2Timestamps.append(vid2StartTime + (cap2.get(cv2.CAP_PROP_POS_MSEC) * 1000000)
     else:
         break
 
@@ -49,7 +45,6 @@ cap2.release()
 #compare the first element in each array to determine which started first
 #take the one that started first and iterate through the timestamps untill they sync
 #do the sme thing with the end to see wich one ends first and where
-
 vid1Start = 0
 vid1End = len(vid1Timestamps) -1
 vid2Start = 0
@@ -77,6 +72,7 @@ while (frame < vid1End or vid2End):
     (status, stitched) = stitcher.stitch([vid1f[vid1Start + frame],vid2f[vid2Start + frame]])
     if status == 0:
         cv2.imshow('Image',stitched)
+        time.sleep(0.033)
     else:
         print("failed to stitch")
 
