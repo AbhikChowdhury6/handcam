ab9d24275514e7b5f96ce240f370425696b78455
AbhikChowdhury6 <abhik.ch6@gmail.com>
2019-02-26 19:44:49 +0000

    lowerd streaming resolution

--- VideoStream.sh
+++ VideoStream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #mplayer -fps 30 -demuxer h264es ffmpeg://tcp://192.168.1.102:2222
-raspivid -t 0 -n -w 1440 -h 1440 $2 $3 $4 $5 -ih -fl -fps 24 -o - | nc -k -l $1
+raspivid -t 0 -n -w 720 -h 720 $2 $3 $4 $5 -ih -fl -fps 30 -o - | nc -k -l $1
