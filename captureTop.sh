1ad7b5670bd3b87b5469509f9f5575cdccd646cd
AbhikChowdhury6 <abhik.ch6@gmail.com>
2019-04-15 20:13:28 +0000

    removed pressure

--- captureTop.sh
+++ captureTop.sh
@@ -8,6 +8,6 @@ trialNumber=$1
 placeToSend=$2
 
 trap 'kill %1;' SIGINT
-bash /home/pi/handcam/VideoCap.sh $trialNumber topCam & python3 /home/pi/handcam/BNOlogging.py $trialNumber & python /home/pi/handcam/logSerialPressure.py $trialNumber 
+bash /home/pi/handcam/VideoCap.sh $trialNumber topCam & python3 /home/pi/handcam/BNOlogging.py $trialNumber #& python /home/pi/handcam/logSerialPressure.py $trialNumber 
 
 bash /home/pi/handcam/send.sh $trialNumber $placeToSend topCam
