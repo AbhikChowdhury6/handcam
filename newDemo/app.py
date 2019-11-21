d3bc5b6c50570f522a34fba32a98f95ed799a173
AbhikChowdhury6 <abhik.ch6@gmail.com>
2019-11-20 20:50:19 -0700

    built the handcam capture code

--- newDemo/app.py
+++ newDemo/app.py
@@ -3,20 +3,87 @@ import json
 from io import BytesIO
 import sqlite3
 import time
+import os
 
 import numpy as np
 import requests
 from flask import Flask, request, jsonify, render_template
 from keras.applications import inception_v3
 from keras.preprocessing import image
+from firebase_util import uploadToFirebase
+
+import cv2
+import glob
+import operator
+
+
+#from flask import Flask, flash, request, redirect, render_template
+#from flask_restful import Resource, Api
+from tensorflow_util import init_tensorflow, infer
 
 # from flask_cors import CORS
 
-app = Flask(__name__)
 
+app = Flask(__name__, static_url_path='')
+app.secret_key = "blah blah"
+#api = Api(app)
+
+#UPLOAD_FOLDER = 'uploads'
+
+def init():
+	print('tensorflow initialization...')
+	init_tensorflow()
+
+init()
+
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+# image processing function that calls the tensorflow oject detection api
+def process(img_path):
+	im = cv2.imread(img_path)
+	print(im.shape)
+	#infer function is the tensorflow object detection API function
+	object_class = infer(im)
+	# results printed to the console. Then the user is redirected back to the main page
+	print("object class::", object_class)
+	return object_class 
 
 # Uncomment this line if you are making a Cross domain request
 # CORS(app)
+
+
+def multiple_img_infer(directory_path, img_ext = ".jpg"):
+	files = glob.glob(directory_path + os.sep + "*" + img_ext)
+	class_details = {'specs':{}, 'card':{}, 'keys':{}}
+	class_count = {'specs':0, 'card':0, 'keys':0}
+	is_object = False
+	for file in files:
+		filename = file[file.rfind('/')+1:file.rfind('.')]
+		im = get_img_arr(file)
+		inference_result = infer(im)
+		print("detection:::", inference_result)
+		object_class = inference_result['class']
+		if object_class in class_count:
+			is_object = True
+			class_count[object_class]+=1
+			class_details[object_class][filename] = inference_result['confidence']
+	if is_object == False:
+		return {"class":"None", "frames":[]}
+	sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
+	final_class = sorted_class_count[0][0]
+	frames = []
+	class_frame_details = class_details[final_class]
+	sorted_frame_details = sorted(class_frame_details.items(), key=operator.itemgetter(1), reverse=True)
+	print(sorted_frame_details)
+	for i in range(3):
+		frames.append(directory_path + "/" + sorted_frame_details[i][0] + img_ext)	 	
+	return {"class": final_class, "frames":frames}
+	
+
+def get_img_arr(img_path):
+	return cv2.imread(img_path)
+
 
 # Testing URL
 @app.route('/hello/', methods=['GET', 'POST'])
@@ -50,28 +117,38 @@ def query_db():
 def image_classifier():
     print("hit the method!")
     # Decoding and pre-processing base64 image
-    rawImg = image.load_img(BytesIO(base64.b64decode(request.form['b64'])),target_size=(480, 480))
-    img = image.img_to_array(rawImg) / 255.
 
-
-    #####save image with uuid and time discriptor
-    URI="data/" + time.asctime() + ".jpg"
+    URI="serverData/" + str(time.time())
     print(URI)
-    rawImg.save(URI)
-    print("tried to save")
-    # this line is added because of a bug in tf_serving(1.10.0-dev)
-    img = img.astype('float16')
+    
+    try:
+        os.makedirs(URI)
+    except OSError:
+        print ("Creation of the directory %s failed" % URI)
+    else:
+        print ("Successfully created the directory %s" % URI)
 
-    # Creating payload for TensorFlow serving request
-    payload = {
-        "instances": [{'input_image': img.tolist()}]
-    }
 
-    # Making POST request
-    r = requests.post('http://localhost:9000/v1/models/ImageClassifier:predict', json=payload)
-    print(r)
-    # Decoding results from TensorFlow Serving server
-    psred = json.loads(r.content.decode('utf-8'))
+    c = int(request.form['count'])
+    i=0
+    for i in range(c):
+        rawImg = image.load_img(BytesIO(base64.b64decode(request.form[str(i)])),target_size=(480, 480))
+        rawImg.save(URI + "/" + str(time.time()) + ".jpg")
+        i = i + 1
+
+
+
+    #*********update to save all of the images to a file
+    #####save image with uuid and time discriptor
+
+
+
+    res = multiple_img_infer(URI)
+    print(res)
+    frames = res["frames"]
+    pred = res["class"]
+    uploadToFirebase(pred, frames)
+
 
     #####form sqlite entry using the predicted class
     with sqlite3.connect("database.db") as con:
@@ -83,5 +160,5 @@ def image_classifier():
 
 
     # Returning JSON response to the frontend
-    return "I saw some ?",(str(pred))
+    return "OK"
 
---
+++ newDemo/tensorflow_util.py
@@ -0,0 +1,113 @@
+import sys
+
+''' PATH TO THE TENSORFLOW INSTALLATION SHOULD BE IN THE PYTHON PATH. See official Tensorflow object detection installation instructions'''
+sys.path.append('/home/peps/Class Files/models/research')
+
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import cv2
+import time
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+from object_detection.utils import label_map_util
+
+# global variables used 
+detection_graph = None
+category_index = None
+session = None
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+
+PATH_TO_CKPT = 'frozen_inference_graph.pb'
+#PATH_TO_CKPT = '<INSERT PATH TO TO THE PB FILE>'
+
+# List of the strings that is used to add correct label for each box.
+
+PATH_TO_LABELS = 'label_map.pbtxt'
+#PATH_TO_LABELS = '<INSERT PATH TO THE LABEL FILE>'
+
+# Number of classes to detect
+NUM_CLASSES = 3
+
+# init function called at the time of flask app startup
+def init_detection_graph():
+    global detection_graph
+    global session
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        session = tf.Session(graph=detection_graph)
+
+# init function called at the time of flask app startup
+def init_label_map():
+    global category_index
+    # Loading label map
+    # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(
+        label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+
+
+def init_tensorflow():
+    print("Tensorflow init started")
+    if detection_graph is None or category_index is None or session is None:
+        print("running initialization.....")
+        init_detection_graph()
+        init_label_map()
+        print("initialization completed")
+    else:
+        print("already initialized::")
+
+# input:: numpy image array
+# output:: Class (String)
+def infer(image_np):
+    print(image_np.shape)
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    start_time = time.time()
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    # Extract image tensor
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Extract detection boxes
+    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    #print(boxes)
+    # Extract detection scores
+    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    # Extract detection classes
+    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    #print(classes)
+    # Extract number of detectionsd
+    num_detections = detection_graph.get_tensor_by_name(
+        'num_detections:0')
+    end_time = time.time()
+    #print('time taken::prerun', end_time - start_time)
+    # Actual detection.
+    start_time = time.time()
+    (boxes, scores, classes, num_detections) = session.run(
+        [boxes, scores, classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+    end_time = time.time()
+    print('time taken::run', end_time - start_time)
+    class_name = "None"
+    confidence = 0.0
+    for index, value in enumerate(classes[0]):
+        #print('index->', index, ' value->', value, 'scores->', scores[0, index])
+        if scores[0, index] > 0.5:
+            class_name = category_index.get(value)['name']
+            confidence = scores[0, index]
+            print("category::", category_index.get(value), "scores::", scores[0, index])
+    #res = ([category_index.get(value) for index,value in enumerate(classes[0]) if scores[0,index] > 0.5])
+    #print(res)
+    return {"class": class_name, "confidence": confidence}
